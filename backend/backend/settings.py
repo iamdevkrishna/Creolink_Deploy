e35290d027047a3ed1e790d59b1149ff55e03d80
Dev Krishna <115032869+iamdevkrishna@users.noreply.github.com>
2026-05-21 11:59:34 +0530

    Fix WhiteNoise middleware and STATIC_ROOT for admin CSS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,7 +54,8 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
     'corsheaders.middleware.CorsMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -148,6 +149,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
