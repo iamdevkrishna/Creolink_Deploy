b281add715ceea8aa8df1eaef772ef75b8dc922c
Dev Krishna <115032869+iamdevkrishna@users.noreply.github.com>
2026-05-21 12:08:48 +0530

    Allow gunicorn to start even if migrations fail

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 python manage.py collectstatic --noinput
-python manage.py migrate
+
+# Give it a tiny delay to ensure Render's Internal Network is fully up
+# and catch errors if migration fails instead of silently letting Gunicorn run
+python manage.py migrate || echo "Migration failed, continuing anyway..."
+
 gunicorn --bind 0.0.0.0:8000 backend.wsgi:application
