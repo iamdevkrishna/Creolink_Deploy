19c02ce16b0220cd5bc02a8b21931ef94bfdde45
Dev Krishna <115032869+iamdevkrishna@users.noreply.github.com>
2026-05-21 12:14:58 +0530

    Force Django to serve media files in production

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 # NEW: Import the JWT Views
 from rest_framework_simplejwt.views import (
@@ -16,8 +17,7 @@ urlpatterns = [
     # NEW: The exact endpoints where React will get the digital keycards
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # Force Django to serve media files in production (Render) without AWS S3
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# Add media URLs for file uploads to work during development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
